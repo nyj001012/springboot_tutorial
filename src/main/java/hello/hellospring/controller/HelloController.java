de5bbf4a4d629e11ebdf5f45287ca788b019ceb0
yena <50291995+nyj001012@users.noreply.github.com>
2024-03-22 12:33:26 +0900

    feat: add files omitted

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,54 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public HelloController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "치킨");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-mvc";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name") String name) {
+        return name + "'s greetings!";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public User helloAPI(@RequestParam(value = "name") String name) {
+        User user = new User(name);
+        return user;
+    }
+
+    static class User {
+        public String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+    }
+}
