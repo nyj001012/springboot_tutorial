6768fbc1cbcb8f7d0d2408da5db6166c674cdf98
yena <50291995+nyj001012@users.noreply.github.com>
2024-03-22 16:29:59 +0900

    feat(controller): add controller for sign-up post

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,13 +1,29 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @GetMapping("/members/sign-up")
     public String getMembersSignUp() {
         return "members/sign-up";
+    }
+
+    @PostMapping("/members/sign-up")
+    public String postMembersSignUp(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
     }
 }
