0db1af86a43e5b76b4650f04fc1b0e9a6c287a20
yena <50291995+nyj001012@users.noreply.github.com>
2024-04-15 10:45:44 +0900

    chore(repository): delete unnecessary annotation

--- src/main/java/hello/hellospring/repository/JDBCTemplateMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JDBCTemplateMemberRepository.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Repository
 public class JDBCTemplateMemberRepository implements MemberRepository {
     private final JdbcTemplate jdbcTemplate;
 
